af78b11680b080b469d6a0438b4b06e6a7ebde09
ovjust <56216004@qq.com>
2022-03-16 17:34:31 +0800

    GetAllCoinsPrice

--- BaiduApi.BaseApi/Btc/BiAnFApi_candle.cs
+++ BaiduApi.BaseApi/Btc/BiAnFApi_candle.cs
@@ -97,5 +97,35 @@ namespace BaiduApi.BaseApi.Btc
                 instrument_id = coinName + "USDT";
             return GetCandles_pair(instrument_id,duration,start, end);
         }
+
+
+        public static string GetAllCoinsPrice()
+        {
+            var url= "https://fapi.binance.com/fapi/v1/ticker/24hr";
+            useSSL();
+            string result = WebRequestHelper.RequestWebClient(url, Encoding.UTF8);
+            return result;
+
+            /*
+             * {
+ 1 "symbol": "BTCUSDT",
+  "priceChange": "-94.99999800",    //24小时价格变动
+ 1 "priceChangePercent": "-95.960",  //24小时价格变动百分比
+  "weightedAvgPrice": "0.29628482", //加权平均价
+ 1 "lastPrice": "4.00000200",        //最近一次成交价
+  "lastQty": "200.00000000",        //最近一次成交额
+  "openPrice": "99.00000000",       //24小时内第一次成交的价格
+  "highPrice": "100.00000000",      //24小时最高价
+  "lowPrice": "0.10000000",         //24小时最低价
+  "volume": "8913.30000000",        //24小时成交量
+  "quoteVolume": "15.30000000",     //24小时成交额
+  "openTime": 1499783499040,        //24小时内，第一笔交易的发生时间
+  "closeTime": 1499869899040,       //24小时内，最后一笔交易的发生时间
+  "firstId": 28385,   // 首笔成交id
+  "lastId": 28460,    // 末笔成交id
+  "count": 76         // 成交笔数
+}
+             * */
+        }
     }
 }
